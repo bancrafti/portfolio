ded10f487af77e7c51684afaecff369e00987922
bancrafti <swishieway@gmail.com>
2025-01-03 23:31:53 +0300

    added all divs

---
+++ folio/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container">
+        <div class="bio"></div>
+        <div class="photo"></div>
+        <div class="tech-stack"></div>
+        <div class="waribu"></div>
+        <div class="contact"></div>
+        <div class="socials"></div>
+    </div>
+</template>
